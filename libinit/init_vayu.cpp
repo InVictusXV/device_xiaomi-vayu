1688872575575ff85889d031c54af6649cbb1fe0
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-05 05:57:10 +0900

    vayu: libinit: set dalvik heap configuration conditionally
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- libinit/init_vayu.cpp
+++ libinit/init_vayu.cpp
@@ -9,6 +9,8 @@
 #define DESCRIPTION "vayu_global-user 11 RKQ1.200826.002 V12.5.2.0.RJUMIXM release-keys"
 #define FINGERPRINT "POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.2.0.RJUMIXM:user/release-keys"
 
+#include <sys/sysinfo.h>
+
 static const variant_info_t vayu_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "vayu",
@@ -37,6 +39,27 @@ static const variant_info_t vayu_eea_info = {
     .nfc = true,
 };
 
+void load_dalvik_properties() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram < 7000ull * 1024 * 1024) {
+        // 6GB RAM
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    } else {
+        // 8GB RAM
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heaptargetutilization", "0.46");
+        property_override("dalvik.vm.heapmaxfree", "48m");
+    }
+
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapminfree", "8m");
+}
+
 static const variant_info_t bhima_info = {
     .hwc_value = "INDIA",
     .sku_value = "bhima",
@@ -59,4 +82,6 @@ static const std::vector<variant_info_t> variants = {
 
 void vendor_load_properties() {
     search_variant(variants);
+
+    load_dalvik_properties();
 }
