f0c5bc70ec295e58d3116639a2add6b4fbc5e4e5
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    vayu: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    [SebaUbuntu]: Cleanup Android.bp and add copyright header
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -379,7 +379,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-     android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    libqti-perfd-client
 
 # Public libraries
 PRODUCT_COPY_FILES += \
