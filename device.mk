8cc5c485d85b584582b2345f81a4fb56de5a8f81
rahul9999xda <rahul9999xda@gmail.com>
2021-05-05 04:33:13 +0000

    vayu: Add prebuilt GoogleCameraGo
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- device.mk
+++ device.mk
@@ -222,6 +222,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# Google Camera Go
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
