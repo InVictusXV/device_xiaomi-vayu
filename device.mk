0969ad751bc7f241aeff8dee3c1f7062da6c07ed
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    vayu: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf

--- device.mk
+++ device.mk
@@ -386,6 +386,10 @@ PRODUCT_COPY_FILES += \
 # Properties
 include $(LOCAL_PATH)/properties/default.mk
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
