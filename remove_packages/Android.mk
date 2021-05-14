ef3bb12c6af764a069505c0afc977ca611387ef7
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    vayu: Remove some Gapps packages at build time
    [fryevia: Rearrange LOCAL_OVERRIDES_PACKAGES]
    [nullxception: cleanup overrides and rename package]
    
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackagesVayu
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Chrome-Stub \
+    Drive \
+    FilesPrebuilt  \
+    Maps \
+    YouTube
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
