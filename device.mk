e6ff42190560769a0df12ca519882177d075308f
Tim Murray <timmurray@google.com>
2021-05-17 15:56:48 -0700

    vayu: Preopt SystemUI
    SystemUI should always be speed-preopted on these devices.
    
    Test: flashed, SysUI is preopted
    Bug: 188465988
    Change-Id: I990ce9d7d8c08b8e80fe4ca0b73c9eb339f065f3

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Preopt packages
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUIGoogle
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
