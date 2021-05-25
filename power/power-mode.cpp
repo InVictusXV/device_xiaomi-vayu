0cc029adf4abc808c8fffd3c7050f31e788cfbf2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-25 13:38:04 +0530

    vayu: Implement extension for kernelspace battery saver
     * this activates kernelspace battery saver via powerhal whenever
       battery saver is enabled in userspace, thereby lowering power
       consumption at kernel-level by disabling boosts and such
    
    Signed-off-by: InVictusXV <aradheakiki123@gmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -16,43 +16,9 @@
 
 #include <aidl/android/hardware/power/BnPower.h>
 #include <android-base/file.h>
-#include <android-base/logging.h>
-#include <linux/input.h>
+#include "power-common.h"
 
-namespace {
-int open_ts_input() {
-    int fd = -1;
-    DIR *dir = opendir("/dev/input");
-
-    if (dir != NULL) {
-        struct dirent *ent;
-
-        while ((ent = readdir(dir)) != NULL) {
-            if (ent->d_type == DT_CHR) {
-                char absolute_path[PATH_MAX] = {0};
-                char name[80] = {0};
-
-                strcpy(absolute_path, "/dev/input/");
-                strcat(absolute_path, ent->d_name);
-
-                fd = open(absolute_path, O_RDWR);
-                if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
-                    if (strcmp(name, "fts_ts") == 0 || strcmp(name, "goodix_ts") == 0 ||
-                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
-                        break;
-                }
-
-                close(fd);
-                fd = -1;
-            }
-        }
-
-        closedir(dir);
-    }
-
-    return fd;
-}
-}  // anonymous namespace
+#define BATTERY_SAVER_NODE "/sys/module/battery_saver/parameters/enabled"
 
 namespace aidl {
 namespace android {
@@ -60,14 +26,11 @@ namespace hardware {
 namespace power {
 namespace impl {
 
-static constexpr int kInputEventWakeupModeOff = 4;
-static constexpr int kInputEventWakeupModeOn = 5;
-
 using ::aidl::android::hardware::power::Mode;
 
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE:
+        case Mode::LOW_POWER:
             *_aidl_return = true;
             return true;
         default:
@@ -77,21 +40,9 @@ bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE: {
-            int fd = open_ts_input();
-            if (fd == -1) {
-                LOG(WARNING)
-                    << "DT2W won't work because no supported touchscreen input devices were found";
-                return false;
-            }
-            struct input_event ev;
-            ev.type = EV_SYN;
-            ev.code = SYN_CONFIG;
-            ev.value = enabled ? kInputEventWakeupModeOn : kInputEventWakeupModeOff;
-            write(fd, &ev, sizeof(ev));
-            close(fd);
+        case Mode::LOW_POWER:
+            ::android::base::WriteStringToFile(enabled ? "Y" : "N", BATTERY_SAVER_NODE, true);
             return true;
-        }
         default:
             return false;
     }
