44296b268b347ac1d4c782cc18359818bde7a6d4
alanndz <alanndz@qmail.id>
2021-07-17 01:19:21 +0000

    vayu: nad: build use gapps
    Signed-off-by: alanndz <alanndz@qmail.id>

--- nad_vayu.mk
+++ nad_vayu.mk
@@ -8,6 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+USE_GAPPS := true
+
 # Inherit some common stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
