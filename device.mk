4eb7df22a88f0d70bcf83312562576062d0ece7e
InVictusXV <aradheakiki123@gmail.com>
2021-07-30 15:46:19 +0700

    Revert "vayu: Bring back AOSP BT stack"
    This reverts commit 26d80ea13f2649eda7d22499d34c458edf472895.

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
     libbundlewrapper \
@@ -148,6 +149,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
