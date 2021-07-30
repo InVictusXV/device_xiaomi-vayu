3efbb19cf2c091515da340f6536dfc25d9a9197f
alanndz <alanndz@qmail.id>
2021-07-30 23:30:59 +0000

    vayu: Dont build power stats mock
    Signed-off-by: alanndz <alanndz@qmail.id>

--- device.mk
+++ device.mk
@@ -343,8 +343,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # Public libraries
 PRODUCT_COPY_FILES += \
