31377f618c46a386cb7422604c3ad6aa94725602
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-10-16 21:59:43 +0530

    vayu: Inherit various aosp makefiles
    Signed-off-by: GrettoRosella <rezarifaldy12@gmail.com>

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -11,8 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
