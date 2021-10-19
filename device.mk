42206fac023312aaf7313c3aee004ba2867ff0dc
kubersharma001 <kubersharma001@gmail.com>
2021-10-19 05:46:59 +0000

    vayu: Temporary changes for bringup
    Change-Id: Ibbc9440d208b9fda342c2754790129a2ca103171

--- device.mk
+++ device.mk
@@ -297,10 +297,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
 # IFAA manager
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
 # IPACM
