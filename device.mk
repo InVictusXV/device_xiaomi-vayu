fbb930cdc0229747b32de2b6d443aff926b22366
kubersharma001 <kubersharma001@gmail.com>
2021-11-02 10:27:32 +0000

    Partially revert "vayu: provide android.hardware.{neuralnetworks@1.3, nfc@1.2} on vendor"
    Change-Id: I369c54a713a526e41e180bd5fbb92445c067d1e0

--- device.mk
+++ device.mk
@@ -456,7 +456,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
-    android.hardware.nfc@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
     NfcNci \
