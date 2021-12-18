6b8c70067d927e3e1ea0f10c09ddc03fcb07a93f
InVictusXV <aradheakiki123@gmail.com>
2021-12-18 00:50:35 +0000

    vayu: Use prebuilt vendor.qti.hardware.btconfigstore
    Decided to use prebuilt vendor.qti.hardware.btconfigstore to make bluetooth working
    
    Signed-off-by: InVictusXV <aradheakiki123@gmail.com>

--- device.mk
+++ device.mk
@@ -104,9 +104,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
