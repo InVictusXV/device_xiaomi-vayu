9f88669d81d62f2b8b006e9a0e819e588d1b7cd7
Rafiester <rafi.haqqi@gmail.com>
2021-12-26 22:15:34 +0700

    vayu: Build Pixel Powerstats
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- device.mk
+++ device.mk
@@ -400,6 +400,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.vayu-libperfmgr \
+    android.hardware.power.stats@1.0-service.xiaomi \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
