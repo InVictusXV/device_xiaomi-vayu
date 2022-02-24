f9ced5e6b82744377e87b5ceb00003703b2fce09
Nauval Rizky <enuma.alrizky@gmail.com>
2022-02-24 19:27:33 +0700

    vayu: parts: Combine pulse notification and raise to wake preference
    ListPreference is make more sense than switches for this

--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -36,13 +36,16 @@ public final class DozeUtils {
     protected static final String CATEG_PICKUP_SENSOR = "pickup_sensor";
     protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
     protected static final String DOZE_ENABLE = "doze_enable";
-    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
-    protected static final String GESTURE_RAISE_TO_WAKE_KEY = "gesture_raise_to_wake";
+    protected static final String GESTURE_PICK_UP_MODE_KEY = "gesture_pick_up_mode";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
     private static final String TAG = "DozeUtils";
     private static final boolean DEBUG = false;
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
+    public static final int PICK_UP_MODE_OFF = 0;
+    public static final int PICK_UP_MODE_PULSE = 1;
+    public static final int PICK_UP_MODE_WAKE = 2;
 
     public static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
@@ -114,12 +117,13 @@ public final class DozeUtils {
                 .getBoolean(gesture, false);
     }
 
-    public static boolean isPickUpEnabled(Context context) {
-        return isGestureEnabled(context, GESTURE_PICK_UP_KEY);
-    }
-
-    public static boolean isRaiseToWakeEnabled(Context context) {
-        return isGestureEnabled(context, GESTURE_RAISE_TO_WAKE_KEY);
+    public static int getPickUpMode(Context context) {
+        try {
+            return Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context)
+                    .getString(GESTURE_PICK_UP_MODE_KEY, "0"));
+        } catch (NumberFormatException ignored) {
+            return 0;
+        }
     }
 
     public static boolean isHandwaveGestureEnabled(Context context) {
@@ -131,7 +135,7 @@ public final class DozeUtils {
     }
 
     public static boolean sensorsEnabled(Context context) {
-        return isPickUpEnabled(context) || isRaiseToWakeEnabled(context) ||
+        return getPickUpMode(context) != PICK_UP_MODE_OFF ||
                 isHandwaveGestureEnabled(context) || isPocketGestureEnabled(context);
     }
 }
