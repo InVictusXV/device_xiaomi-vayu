a911cc712525470946c547407cfd2c9235d81ea0
Nauval Rizky <enuma.alrizky@gmail.com>
2022-03-06 19:11:39 +0700

    vayu: Revise thermal engine and HAL setup
    - Move to thermal_info_config.json as per default pixel
    - Sync ThrottlingSeverity.SHUTDOWN for CPU and GPU from coral
    - Move throttling mechanism to thermal engine
    
    Co-authored-by: alanndz <alanndz7@gmail.com>

--- device.mk
+++ device.mk
@@ -497,7 +497,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal_info_config_normal.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_normal.json
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Touchfeature
 PRODUCT_PACKAGES += \
