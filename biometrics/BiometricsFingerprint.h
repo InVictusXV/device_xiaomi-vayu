838c8f6b1a144a06a341de777d08c96a98c9d480
Chenyang Zhong <zhongcy95@gmail.com>
2022-04-20 12:46:23 +0000

    vayu: fingerprint: apply a boost after fp is authenticated
    Connect to the aidl power HAL extension to apply a 2ms LAUNCH boost
    after fingerprint is authenticated successfully. This should speed
    up the unlocking process and avoid jank in the ripple animation.
    IPowerExt is used instead of the generic android.hardware.power.IPower
    so that we are free to use any custom power hints that are not
    defined in the generic aidl Boost type.
    
    Some code was adapted from
    hardware/google/graphics/common/libhwc2.1/libdevice/
    
    Test: boost hint is supported and libperfmgr applies the hint
    [rafiester: Adapt for vayu]
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- biometrics/BiometricsFingerprint.h
+++ biometrics/BiometricsFingerprint.h
@@ -26,6 +26,22 @@
 #include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
 #include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
 
+namespace aidl {
+namespace google {
+namespace hardware {
+namespace power {
+namespace extension {
+namespace pixel {
+
+class IPowerExt;
+
+} // namespace pixel
+} // namespace extension
+} // namespace power
+} // namespace hardware
+} // namespace google
+} // namespace aidl
+
 namespace android {
 namespace hardware {
 namespace biometrics {
@@ -69,6 +85,11 @@ public:
 
 private:
     static xiaomi_fingerprint_device_t* openHal(const char *class_name);
+    int32_t connectPowerHalExt();
+    int32_t checkPowerHalExtBoostSupport(const std::string &boost);
+    int32_t sendPowerHalExtBoost(const std::string &boost, int32_t durationMs);
+    int32_t isBoostHintSupported();
+    int32_t sendAuthenticatedBoostHint();
     static void notify(const fingerprint_msg_t *msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
@@ -78,6 +99,10 @@ private:
     std::mutex mClientCallbackMutex;
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
     xiaomi_fingerprint_device_t *mDevice;
+
+    bool mBoostHintIsSupported;
+    bool mBoostHintSupportIsChecked;
+    std::shared_ptr<aidl::google::hardware::power::extension::pixel::IPowerExt> mPowerHalExtAidl;
 };
 
 }  // namespace implementation
